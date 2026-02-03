269fd5c31a3a2fa45fa17753bd9ef0564e041b5f
senuraya <senurah30@gmail.com>
2026-02-03 17:41:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CQVhgKLb#VfvRSZB7c2lTbQfVGtKkfrSFO9R4QokFGK2Guy-HjVs",
+SESSION_ID: process.env.SESSION_ID || "aYFmEZKR#CDxKAA0iB57skbkzTG26fBbSqqkUSOTihgTRvKAYrXk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/senuraya/Senura_Md/blob/main/images/ChatGPT%20Image%20Aug%2019,%202025,%2005_22_42%20PM.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 SENURA-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
